7f1c27fdd55e45c735b377de096e27d70e886fe4
Anita Nomula <anitavvs220@gmail.com>
2024-02-05 14:46:03 -0500

    Added another function to existing Hello World code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
 print("Hello, World!")
+
+#To define a function that take two integers 
+# and return the sum of those two numbers
+def add(number1,number2):
+return number1+number2
+
+#initializing the variables
+num1 = 10
+num2 = 5
+
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = add(num1,num2)
+
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1,
+						num2, sum_of_twonumbers))
